7761dc9f884fef4132e883384c7253069ddd6fb2
David Heath <heath.davidanthony@gmail.com>
2014-01-31 12:07:56 -0500

    Added Dfa comments

--- src/dfa/Dfa.java
+++ src/dfa/Dfa.java
@@ -17,9 +17,24 @@ public class Dfa {
     states.put(id, row);
   }
 
+  /**
+   * Changes the current state of the automaton. It is the responsibility
+   * of the user to query tokens, token types, and accept states. If the
+   * current state becomes -1 it represents a lexical error.
+   */
   public void changeState(char c) {
     currState = states.get(currState).getDestination(c);
     currToken += c;
+  }
+
+  /**
+   * It is the responsibility of the user to retrieve tokens and token
+   * types once an acceptance state is reached. The user is also
+   * responsible for resetting the Dfa to allow more tokens to be
+   * generated.
+   */
+  public boolean isInAcceptState() {
+    return states.get(currState).isAcceptState();
   }
 
   public String getTokenType() {
@@ -28,10 +43,6 @@ public class Dfa {
 
   public String getToken() {
     return currToken.substring(0, currToken.length()-1);
-  }
-
-  public boolean isInAcceptState() {
-    return states.get(currState).isAcceptState();
   }
 
   public boolean isSpace() {
