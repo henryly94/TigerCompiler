2e6fed836ef0329074d0bf91a879f22c1aa06d6b
David Heath <heath.davidanthony@gmail.com>
2014-03-23 17:45:16 -0400

    Small refactorings.

--- src/grammar/GrammarBuilder.java
+++ src/grammar/GrammarBuilder.java
@@ -9,27 +9,51 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GrammarBuilder {
+
+  private List<State> states;
+  private int currentStateNum;
+  private String[] tokens;
+  private StringSplitter ss;
+  private final FileScraper fs;
+
+  public GrammarBuilder() {
+    fs = new FileScraper();
+    ss = new StringSplitter(' ');
+  }
+
   public List<State> buildFrom(String filename) {
-    FileScraper fs = new FileScraper();
-    String[] lines = fs.read(filename);
-    StringSplitter ss = new StringSplitter(' ');
-
-    List<State> states = new ArrayList<State>();
-    GrammarState state;
-    String[] tokens;
-    int gotoState = 0;
-    for (String line : lines) {
-      tokens = ss.split(line);
-      String name = tokens[0].substring(0, tokens[0].length() - 1);
-      for (int i = 1; i < tokens.length; i++) {
-        state = new GrammarState(name, -2);
-        state.addDestination(tokens[i], ++gotoState);
-        states.add(state);
-      }
-      state = new GrammarState(name, -1);
-      states.add(state);
-    }
-
+    String[] lines = initialize(filename);
+    for (String line : lines)
+      getStatesFromLine(line);
     return states;
+  }
+
+  private String[] initialize(String filename) {
+    currentStateNum = 0;
+    states = new ArrayList<State>();
+    return fs.read(filename);
+  }
+
+  private void getStatesFromLine(String line) {
+    tokens = ss.split(line);
+    String stateName = getStateName();
+    for (int tokenNum = 1; tokenNum < tokens.length; tokenNum++)
+      generateIntermediateState(stateName, tokenNum);
+    generateReturnState(stateName);
+  }
+
+  private String getStateName() {
+    return tokens[0].substring(0, tokens[0].length() - 1);
+  }
+
+  private void generateIntermediateState(String name, int tokenNum) {
+    GrammarState state = new GrammarState(name, -2);
+    state.addDestination(tokens[tokenNum], ++currentStateNum);
+    states.add(state);
+  }
+
+  private void generateReturnState(String name) {
+    GrammarState state = new GrammarState(name, -1);
+    states.add(state);
   }
 }
